8596e7c1c45ab74d5c857c61ee827cbefff3c290
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-27 18:22:01 -0700

    switch to cmd-line options, add a bunch of features

--- python/countEventsPerSample.py
+++ python/countEventsPerSample.py
@@ -6,31 +6,53 @@
 # davide.gerbaudo@gmail.com
 # 2013-03
 
-import glob, os, re, sys
+import glob, optparse, os, re, sys
+import sampleUtils
 
 import ROOT as r
-r.gROOT.SetBatch(1)
-r.gErrorIgnoreLevel = 9999 # suppress messages about missing dict
-                           # (can't get rid of the 'duplicate' ones?)
-treeName = 'susyNt'
-binLabel='Initial'
-#binLabel='Hot Spot'
-printBinLabels=False       # usually we count the 'Initial' numbers, but there are others available
+r.PyConfig.IgnoreCommandLineOptions = True # don't let root steal your cmd-line options
+r.gROOT.SetBatch(1)                        # go batch!
+r.gErrorIgnoreLevel = 9999                 # suppress messages about missing dict
+                                           # (can't get rid of the 'duplicate' ones?)
 
-if len(sys.argv) < 2 :
-    print "Usage : %s targetDir [-r regexp-filter]" % sys.argv[0]
-    print "Example :"
-    print "%s /gdata/atlas/ucintprod/SusyNt/mc12_n0127 -r 'Sherpa_CT10.*(WW|VV)\'" % sys.argv[0]
-    sys.exit()
-target = sys.argv[1]
-regex = sys.argv[3] if len(sys.argv)>3 and sys.argv[2]=='-r' else None
 
-sampleDirs = sorted([t for t in glob.glob(target+'/*')
+
+defaultBinLabel = 'Initial'
+rawHistoName, genHistoName = 'rawCutFlow', 'genCutFlow'
+usage = ("Usage : %prog [options] args"
+         "\n Examples :"
+         "\n %prog  -r 'Sherpa_CT10.*(WW|VV)' /gdata/atlas/ucintprod/SusyNt/mc12_n0127/"
+         "\n %prog  -r 'n0127b' /gdata/atlas/ucintprod/SusyNt/*_n0127/"
+         )
+
+parser = optparse.OptionParser(usage = usage)
+
+parser.add_option("--gen", dest="gen", default=False, action="store_true", help='weighted counts (default raw counts)')
+parser.add_option("-b", "--bin-label", dest="binlabel", default=defaultBinLabel)
+parser.add_option("-r", "--regex", dest="regex", default=None)
+parser.add_option("--print-bin-labels", dest="printbinlabels", default=False, action="store_true")
+parser.add_option("--full-names", dest="fullnames", default=False, action="store_true")
+parser.add_option("-v", "--verbose", dest="verbose", default=False, action="store_true")
+
+(options, args) = parser.parse_args()
+inputdirs       = args
+gen             = options.gen
+histoName       = genHistoName if gen else rawHistoName
+binLabel        = options.binlabel
+printBinLabels  = options.printbinlabels
+regex           = options.regex
+fullnames       = options.fullnames
+verbose         = options.verbose
+
+if verbose :
+    print "Using the following options:"
+    print '\n'.join(["%s : %s"%(s, eval(s)) for s in ['inputdirs','histoName','binLabel','printBinLabels','regex','fullnames']])
+
+sampleDirs = sorted([t for t in [d  for inputdir in inputdirs for d in glob.glob(inputdir+'/*')]
                      if os.path.isdir(t) and (re.search(regex, t) if regex else True)])
 
-def getProcessedEvents(filename, histoName='rawCutFlow', binLabel=binLabel, printBinLabels=printBinLabels) :
+def getProcessedEvents(filename, histoName='', binLabel='', printBinLabels=False) :
     f = r.TFile.Open(filename)
-    histoName = 'genCutFlow'
     histo = f.Get(histoName)
     h = histo
     if printBinLabels : print [h.GetXaxis().GetBinLabel(i) for i in range(1,1+h.GetNbinsX())]
@@ -38,10 +60,12 @@ def getProcessedEvents(filename, histoName='rawCutFlow', binLabel=binLabel, prin
     f.Close()
     return processedEvents
 
-print '-'*60
+shortname = sampleUtils.minimalSampleName
+
+if verbose : print '-'*60
 for sd in sampleDirs :
     container = os.path.basename(sd)
     files = glob.glob(sd+'/'+'*.root*')
-    nEntries = sum([getProcessedEvents(f) for f in files])
-    print container + ' : '+str(nEntries)
+    nEntries = sum([getProcessedEvents(f, histoName, binLabel, printBinLabels) for f in files])
+    print (container if fullnames else shortname(container)) + ' : '+str(nEntries)
     continue
---
+++ python/sampleUtils.py
@@ -0,0 +1,28 @@
+#!/bin/env python
+
+# Utility functions to manage and navigate samples
+#
+# davide.gerbaudo@gmail.com
+# Mar 2013
+
+import re
+
+def parseSusyntSampleName(samplename='', verbose=False) :
+    "Parse a sample name and split it into meaningful parts"
+    # template sample name:
+    # user.sfarrell.mc12_8TeV.174834.Sherpa_CT10_llll_ZZ.SusyNt.e1721_s1581_s1586_r3658_r3549_p1328_n0127
+    rep = re.compile('user\.(?P<user>.*?)\.'   # user (non greedy)
+                     +'(?P<sample>.*?)\.'      # sample (non greedy)
+                     +'SusyNt\.'
+                     +'(?P<tag>.*)') # tag is the last token (greedy), everything that's left
+    match = rep.search(samplename)
+    if not match :
+        if verbose : print "cannot parse '%s'"%samplename
+        return
+    return dict([(k, match.group(k)) for k in ['user','sample','tag']])
+
+def nttag(alltags) :
+    return alltags[alltags.rfind('_n')+1:]
+
+def minimalSampleName(samplename='',verbose=False) :
+    return parseSusyntSampleName(samplename,verbose)['sample']
