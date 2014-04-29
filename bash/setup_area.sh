8e3d4081d3d502578c13559c9fe12d75309b1bfb
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-04-29 14:53:05 -0700

    preparing for n0154

--- bash/setup_area.sh
+++ bash/setup_area.sh
@@ -22,7 +22,7 @@
 #
 # davide.gerbaudo@gmail.com, Mar 2013
 
-TAG="n0150"
+TAG="n0154"
 
 PROD_DIR="prod_${TAG}"
 SUBM_DIR="subm_${TAG}"
@@ -32,10 +32,10 @@ echo "Starting                          -- `date`"
 mkdir ${PROD_DIR}
 cd    ${PROD_DIR}
 
-svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/SUSYTools-00-03-14               SUSYTools
-svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/MultiLep/tags/MultiLep-01-06-04    MultiLep
-svn co svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/UCIrvine/SUSYAnalysis/SusyNtuple/tags/SusyNtuple-00-01-06    SusyNtuple
-svn co svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/UCIrvine/SUSYAnalysis/SusyCommon/tags/SusyCommon-00-01-04    SusyCommon
+svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/SUSYTools-00-03-19               SUSYTools
+svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/MultiLep/tags/MultiLep-01-06-06    MultiLep
+git clone git@github.com:gerbaudo/SusyNtuple.git; cd SusyNtuple; git checkout SusyNtuple-00-01-09    ; cd -
+git clone git@github.com:gerbaudo/SusyCommon.git; cd SusyCommon; git checkout SusyCommon-00-01-04-02 ; cd -
 
 sed -i '/asetup/s/setup/slc5\,setup/1' MultiLep/installscripts/setup_area.sh # needed for slc6 nodes
 source MultiLep/installscripts/install_script.sh
@@ -43,7 +43,7 @@ source MultiLep/installscripts/install_script.sh
 echo "Done compiling                    -- `date`"
 
 cd ..
-svn co svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/UCIrvine/SUSYAnalysis/SusyCommon/trunk/grid ${SUBM_DIR}
+git clone git@github.com:gerbaudo/SusyCommon.git ${SUBM_DIR}
 cd    ${SUBM_DIR}
 
 localSetupPandaClient
