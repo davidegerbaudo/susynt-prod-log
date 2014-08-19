ae2ac2756b4404db422d1333818bb83a256a354b
gerbaudo <davide.gerbaudo@gmail.com>
2014-08-19 05:34:43 +0200

    add scripts to dl lists and tags (wraps downloadDataset.sh)

---
+++ bash/downloadDatasetList.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script downloads all the datasets from a list.txt
+# It just wraps downloadDataset.sh
+#
+# davide.gerbaudo@gmail.com
+# Aug 2014
+
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function download_filelist {
+    local filelist=${1}
+    local line
+    # drop empty and comment lines
+    for line in $( cat ${filelist} | sed '/^\s*$/d' | sed '/^\#/d' )
+    do
+        ${PROGDIR}/downloadDataset.sh ${line}
+    done
+}
+
+function main {
+    if [[ ! ("$#" == 1) ]]
+    then
+        echo "1 argument required (e.g. user.foo.baz_n0154/), $# provided"
+        exit 1
+    fi
+    download_filelist $*
+}
+
+#___________________________________________________________
+main $*
---
+++ bash/downloadTag.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# This script downloads all the datasets for a given SusyNtuple production tag.
+# Setup DQ2 and voms proxy before running this.
+#
+# davide.gerbaudo@gmail.com
+# Feb 2013
+
+readonly PROGNAME=$(basename $0)
+# see http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+readonly PROGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function prepare {
+    local tag=${1}
+    echo "Preparing directories and lists for ${tag}"
+    ${PROGDIR}/updateLists.sh ${tag} data
+    ${PROGDIR}/updateLists.sh ${tag} mc
+    ${PROGDIR}/updateLists.sh ${tag} susy
+}
+
+function download_tag {
+    local tag=${1}
+    echo "Downloading ${tag}"
+    cd data12_${tag}/
+    ${PROGDIR}/downloadDatasetList.sh data12.txt
+    cd ../
+    cd mc12_${tag}/
+    ${PROGDIR}/downloadDatasetList.sh mc12.txt
+    cd ../
+    cd susy_${tag}/
+    ${PROGDIR}/downloadDatasetList.sh susy.txt
+    cd ../
+    echo "done"
+}
+
+#___________________________________________________________
+function main {
+    if [[ ! ("$#" == 1) ]]
+    then
+        echo "1 argument required (e.g. n0154), $# provided"
+        exit 1
+    fi
+    prepare $*
+    download_tag $*
+}
+#___________________________________________________________
+main $*
