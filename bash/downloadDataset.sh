6671bbfa9a0b476acc6f94934faca170fa72fa62
gerbaudo <davide.gerbaudo@gmail.com>
2014-08-19 03:24:36 +0200

    downloadDataset.sh initial version

---
+++ bash/downloadDataset.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Script to download a dataset
+
+# This script is just a wrapper around dq2-get to download all the
+# root files and put them in a directory with name=dataset_name.
+
+# Based on Steve's (sfarrell@cern.ch) NtDownload.sh
+# davide.gerbaudo@gmail.com
+# Aug 2014
+
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function help {
+    echo "Usage: ${PROGNAME} dataset [other-dq2-options]"
+}
+
+# Determine from the dataset name the name of the destination directory
+function dest_dir_from_datasetname {
+    local dataset=${1}
+    local jedi_style_suffix="_susyNt.root" # dropping the suffix introduced by jedi will make things bk-compatible
+    local dest_dir=${dataset/${jedi_style_suffix}/}
+    echo "${dest_dir}"
+}
+
+function mkdir_if_needed {
+    local dest_dir=${1}
+    if [ ! -d ./${dest_dir} ]; then
+        echo "Creating output directory"
+        mkdir ${dest_dir}
+        chmod g+rw ${dest_dir}
+    fi
+}
+
+function main {
+    if [ $# -lt 1 ]; then { help; exit 1; } fi
+    local dataset=${1}
+    shift
+    local other_options=""
+    while [[ $# > 0 ]]
+    do
+        other_options="${other_options} $1"
+        shift
+    done
+    local destination_dir=$(dest_dir_from_datasetname ${dataset})
+    local dq2_default_options=""
+    dq2_default_options+=" --threads=3,5" # concurrent datasets,files
+    dq2_default_options+=" --no-directories"
+    dq2_default_options+=" --files=*.root*"
+    dq2_default_options+=" --to-here=${destination_dir}"
+    
+    mkdir_if_needed ${destination_dir}
+    local cmd="dq2-get ${dq2_default_options} ${other_options} ${dataset}"
+    echo ${cmd}
+    ${cmd}
+}
+#___________________________________________________________
+
+main $*
